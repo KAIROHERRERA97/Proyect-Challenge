fc8a93d0859ff924c978de44e63e6f2a6bd3a8ab
KAIROHERRERA97 <136655999+KAIROHERRERA97@users.noreply.github.com>
2024-02-15 13:27:50 -0500

    fix: Detail element

--- src/components/elements/detail.element.tsx
+++ src/components/elements/detail.element.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { Button } from "../ui/button";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Alert, AlertDescription } from "@/components/ui/alert";
 
 function DetailElement() {
   return (
